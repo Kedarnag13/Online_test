7e395d62c42b0d7a7d95d51f566c71695a58bb9e
praveenmenon <pmenon@qwinixtech.com>
2016-02-08 23:21:57 +0530

    [master]adds handler to main

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/Kedarnag13/Online_test/api/v1/controllers/account"
 	"github.com/gorilla/mux"
-	"github.com/kedarnag13/Online_test/api/v1/controllers/account"
 	"log"
 	"net/http"
 )
@@ -11,6 +11,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/sign_up", account.User.Create).Methods("POST")
 	// HTTP Listening Port
-	log.Println("main : Started : Listening on: http://localhost:3020 ...")
-	log.Fatal(http.ListenAndServe("0.0.0.0:3020", nil))
+
+	http.Handle("/", r)
+	log.Println("main : Started : Listening on: http://localhost:3000 ...")
+	log.Fatal(http.ListenAndServe("0.0.0.0:3000", nil))
 }
