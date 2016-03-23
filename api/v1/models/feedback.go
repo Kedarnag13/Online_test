bded59af34e2efa755ebe8d44c5bf8793c922e49
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2016-03-23 18:48:56 +0530

    [develop feedback] Changes to the datatype for description.

--- api/v1/models/feedback.go
+++ api/v1/models/feedback.go
@@ -4,7 +4,7 @@ type Feedback struct {
 	Verbal_section string `valid:"alphanum,required"`
 	Logical_section string `valid:"alphanum,required"`
 	Aptitude_section string `valid:"alphanum,required"`
-	Description string `valid:"alphanum"`
+	Description string `valid:"duck"`
 }
 
 type FeedbackResponse struct {
