7405cff5410c4a16ba810e4f58d7d1d3941672e7
Kedarnag Mukanahallipatna <ksatish@qwinixtech.com>
2016-04-15 00:29:06 +0530

    Sending the email to respective recipients.

--- scripts/send_email.sh
+++ scripts/send_email.sh
@@ -1,1 +1,2 @@
 echo "Please find the attachment of the results" | mutt -a "/tmp/results.csv" -s "Online Test Results" -- ksatish@qwinix.io
+mail -s "Test Email from Online Test"  ksatish@qwinix.io < /dev/null
